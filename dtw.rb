b56675fe00c2f5783ad8221a2b0c9ac5f4aa1e27
Frederico <frederico.marques@live.com>
2015-11-19 18:35:51 -0200

    completed todo's

--- dtw.rb
+++ dtw.rb
@@ -14,10 +14,20 @@ class DTW
 
     (1 .. @a.length).each do |i|
       (1 ..@b.length).each do |j|
-        cost = d(a[i], b[j]) #todo d()
-        dtw[i][j] = cost + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1]) #todo min()
+        cost = d(a[i], b[j])
+        dtw[i][j] = cost + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
       end
     end
     dtw[@a.length][@b.length]
+  end
+
+  def d(a, b)
+    (a - b)**2
+  end
+
+  def min(a, b, c)
+    a if(a < b and a < c)
+    b if(b < a and b < c)
+    c if(c < b and c < a)
   end
 end
