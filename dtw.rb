fad2183a60cab20fb1340b639f1c19ecf5a1026a
Kasama <ralegro@bol.com.br>
2015-11-20 10:58:45 -0200

    test results in results.txt changed name of some variables to match ruby style and be coherent with its functionality

--- dtw.rb
+++ dtw.rb
@@ -1,31 +1,32 @@
 class DTW
-  attr_accessor :a, :b
+  attr_accessor :test, :base
   def initialize(a, b)
-    @a = a
-    @b = b
+    @test = a
+    @base = b
   end
 
   def dtw_distance(p)
 
     p = p/100 if p > 1
 
-    w = p*(@b.length)
+    w = p*(@base.length)
 
-    w = max(w, abs(@a.length - @b.length))
+    w = max(w, abs(@test.length - @base.length))
+    w = w.to_i
 
-    dtw = Array.new(@a.length + 1) { Array.new(@b.length + 1, Float::INFINITY) }
+    dtw = Array.new(@test.length + 1) { Array.new(@base.length + 1, Float::INFINITY) }
     dtw[0][0] = 0
 
-    (1 .. @a.length).each do |i|
-      (max(1, i-w) .. min(@b.length, i+w, Float::INFINITY)).each do |j|
-        cost = d(a[i-1], b[j-1])
+    (1 .. @test.length).each do |i|
+      (max(1, i-w) .. min2(@base.length, i+w)).each do |j|
+        cost = distance(a[i-1], b[j-1])
         dtw[i][j] = cost + min(dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1])
       end
     end
-    dtw[@a.length][@b.length]
+    dtw[@test.length][@base.length]
   end
 
-  def d(a, b)
+  def distance(a, b)
     (a - b)**2
   end
 
@@ -35,6 +36,11 @@ class DTW
     ret = b if(b < a and b < c)
     ret = c if(c < b and c < a)
     ret
+  end
+
+  def min2(a, b)
+    return a if a < b
+    b
   end
 
   def max(a, b)
--- main.rb
+++ main.rb
@@ -1,11 +1,10 @@
 require './file_reader.rb'
 require './dtw.rb'
 
-def get_dtw(test_line, base)
+def get_dtw(test_line, base, p = 1)
   min = Float::INFINITY
   index = -1
 
-  p = 0
   base.each_with_index do |l, i|
     dtw = DTW.new(test_line[:values], l[:values])
     if (d = dtw.dtw_distance(p)) < min # p is the percentage
@@ -19,15 +18,22 @@ end
 test_file = './teste.txt'
 base_file = './treino.txt'
 reader = FileReader.new test_file, base_file
-total = 0
-hits = 0
 
-puts('#########################################################################################################')
-reader.test.each_with_index do |l, i|
-  putc('#') if i % 10 == 0
-  index = get_dtw(l, reader.base)
-  hits += 1 if l[:class] == reader.base[index][:class]
-  total += 1
+percentages = [0, 0.05, 0.1, 0.2, 0.5, 1]
+
+percentages.each do |p|
+  total = 0
+  hits = 0
+
+  puts "Running for #{p*100}%"
+  puts '#########################################################################################################'
+  reader.test.each_with_index do |l, i|
+    putc '#' if i % 10 == 0
+    index = get_dtw(l, reader.base, p)
+    hits += 1 if l[:class] == reader.base[index][:class]
+    total += 1
+  end
+  puts "\nPercetage: #{p*100}%"
+  puts "hits: #{hits}\ntotal: #{total}"
+  puts "eficiencia = #{hits.to_f/total}"
 end
-puts("\nhits: #{hits}\ntotal: #{total}")
-puts("eficiencia = #{hits.to_f/total}")
