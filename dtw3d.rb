86e31ca9af49f2e76d9cdfa1bdb5f55b45a2791b
Kasama <ralegro@bol.com.br>
2015-11-23 16:17:14 -0200

    fixed 3d results

--- dtw3d.rb
+++ dtw3d.rb
@@ -1,8 +1,8 @@
 class DTW3D < DTW
 
-  def distance(a, b)
-    d = ((a[0]+b[0])**2) + ((a[1]+b[1])**2)
-    d = d + ((a[2]+b[2])**2)
+  def distance(a, b) #mudei para menos nas duas linhas de baixo
+    d = ((a[0]-b[0])**2) + ((a[1]-b[1])**2)
+    d = d + ((a[2]-b[2])**2)
     Math.sqrt(d)
   end
 end
